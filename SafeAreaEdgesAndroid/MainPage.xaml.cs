901ef81335f27500f056b1e99a92603e52720c13
bebenbricolo <benoittarrade@hotmail.fr>
2026-07-23 12:59:27 +0200

    Overloaded the pages to better reproduce the bug, even with MAUI 10.0.90
    Signed-off-by: bebenbricolo <benoittarrade@hotmail.fr>

--- SafeAreaEdgesAndroid/MainPage.xaml.cs
+++ SafeAreaEdgesAndroid/MainPage.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace SafeAreaEdgesAndroid;
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace SafeAreaEdgesAndroid;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+
+        // Decoding MAUI versioning system
+        var mauiAssembly = typeof(Microsoft.Maui.IApplication).Assembly;
+        var mauiProductVersionRaw = mauiAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion.ToString();
+        var semVerPattern = "([\\d]+\\.[\\d]+\\.[\\d]+)";
+        var mauiProductVersion = Regex.Match(mauiProductVersionRaw, semVerPattern);
+
+        MauiVersionLabel.Text = mauiProductVersion.Value;
     }
 
     private void OnCounterClicked(object? sender, EventArgs e)
